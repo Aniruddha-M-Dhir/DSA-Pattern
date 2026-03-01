9944ee259c84caf370677322357bd2bdc72d8074
Aniruddha-M-Dhir <aniruddhamdhir@gmail.com>
2026-02-28 23:23:27 -0800

    mergesort

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,34 +1,68 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// public class InsertionSort {
+//     public static void insertionSort(int[] arr, int n) {
+//         for (int i = 0; i <= n - 1; i++) {
+//             int j = i;
+//             while (j > 0 && arr[j - 1] > arr[j]) {
+//                 int temp = arr[j];
+//                 arr[j] = arr[j - 1];
+//                 arr[j - 1] = temp;
+//                 j--;
+
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the num of elements in array or length: ");
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         insertionSort(arr, n);
+//         System.out.println("sorted; ");
+//         for (int i = 0; i < n; i++) {
+//             System.out.println(
+//                     arr[i]);
+//         }
+
+//     }
+
+// }
+
+import java.util.*;
 
 public class InsertionSort {
     public static void insertionSort(int[] arr, int n) {
         for (int i = 0; i <= n - 1; i++) {
             int j = i;
             while (j > 0 && arr[j - 1] > arr[j]) {
-                int temp = arr[j];
+                int temp = j;
                 arr[j] = arr[j - 1];
                 arr[j - 1] = temp;
                 j--;
-
             }
+
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the num of elements in array or length: ");
+        System.out.println("enter the number of elements in an array:");
         int n = sc.nextInt();
         int[] arr = new int[n];
+        System.out.println("enterthe elements ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         insertionSort(arr, n);
-        System.out.println("sorted; ");
+        System.out.println(" array: ");
         for (int i = 0; i < n; i++) {
-            System.out.println(
-                    arr[i]);
+            System.out.println(arr[i]);
         }
 
     }
-
 }
---
+++ MergeSort.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class MergeSort {
+    // merge funtion
+    public static void merge(int[] arr, int low, int mid, int high) {
+
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        int left = low; // index 0 of first half of array
+        int right = mid + 1; // index 0 of the second half of array
+
+        // Merge the two sorted array
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        // remaining elements of left half
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        // remaining elements of right half
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // put temp to original array
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+
+    }
+
+    // recursive funtion
+    public static void mS(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (low + high) / 2;
+
+        mS(arr, low, mid);
+        mS(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    // main mergesort funtion
+    public static void mergeSort(int[] arr, int n) {
+        mS(arr, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        mergeSort(arr, n);
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
