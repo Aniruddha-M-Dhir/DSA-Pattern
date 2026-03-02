81260e25140e080344e0944de27257f70490f07c
Aniruddha-M-Dhir <aniruddhamdhir@gmail.com>
2026-03-01 22:48:27 -0800

    qucksort added

---
+++ quickSort.java
@@ -0,0 +1,6 @@
+// quicksort
+// rules:
+// 1. pick a pivot and put it in its correct place.
+// 2. now, put smaller elements to left of pivot, and larger elements to right
+// of pivot.
+// repeat for left side of pivot and right side of pivot
