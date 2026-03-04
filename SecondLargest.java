a1737505d580d0bba1bdffccaedcd89d12b09003
Aniruddha-M-Dhir <aniruddhamdhir@gmail.com>
2026-03-03 23:26:29 -0800

    second largest added

---
+++ SecondLargest.java
@@ -0,0 +1,6 @@
+// the second largest can be returned by sorting, and returning the n-2 element. 
+// there is a better approach. since the brute will give O(NlogN)
+
+public class SecondLargest {
+
+}
