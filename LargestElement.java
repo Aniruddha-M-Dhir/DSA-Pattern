77070971f8dd7c8cf5c1108ec48527ea57a62169
Aniruddha-M-Dhir <aniruddhamdhir@gmail.com>
2026-03-04 22:56:54 -0800

    largest element updated

--- LargestElement.java
+++ LargestElement.java
@@ -1,3 +1,4 @@
+import java.util.*;
 // // Largest element in an array
 // class Solution {
 // public int largestElement(int[] nums) {
@@ -12,3 +13,30 @@
 
 // }
 // }
+public class LargestElement {
+    public static int largest(int[] arr, int n) {
+        int largest = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        return largest;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of elements ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println(";eneter the elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = largest(arr, n);
+        System.out.println(result);
+
+    }
+
+}
