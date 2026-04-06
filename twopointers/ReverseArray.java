df41599ed02f6eed62f7ea7e26f8029a09f7ae88
Aniruddha-M-Dhir <aniruddhamdhir@gmail.com>
2026-04-06 14:22:22 -0700

    practice

--- twopointers/ReverseArray.java
+++ twopointers/ReverseArray.java
@@ -14,17 +14,31 @@ public class ReverseArray {
     // end--;
     // }
     // }
+    // public static void reverseArray(int[] arr, int n) {
+    // int start = 0;
+    // int end = n - 1;
+    // while (start < end) {
+    // int temp = arr[start];
+    // arr[start] = arr[end];
+    // arr[end] = temp;
+    // start++;
+    // end--;
+    // }
+
+    // }
+
     public static void reverseArray(int[] arr, int n) {
         int start = 0;
         int end = n - 1;
-        while (start < end) {
+        while (start <= end) {
             int temp = arr[start];
+
             arr[start] = arr[end];
             arr[end] = temp;
             start++;
             end--;
-        }
 
+        }
     }
 
     public static void main(String[] args) {
